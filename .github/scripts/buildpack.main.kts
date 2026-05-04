fa1ff3de1bead18073a285afe8594f103df1ccdd
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-04 20:24:42 +0200

    remove printlns

--- .github/scripts/buildpack.main.kts
+++ .github/scripts/buildpack.main.kts
@@ -19,13 +19,9 @@ data class PackEntry(val id: String, val name: String)
 val root = __FILE__.toPath().toAbsolutePath().parent.parent.parent
 val destination = root.resolve("dest").createDirectories()
 
-println("1")
-
 Gson().fromJson(root.resolve("packs.json").readText(), Array<PackEntry>::class.java).forEach { (id, name) ->
     val sourceFolder = root.resolve(id)
     val zipFile = root.resolve(destination).resolve("${name}.zip")
-
-    println("2 $id")
 
     if (sourceFolder.exists() && sourceFolder.isDirectory()) {
         println("Zipping $id -> ${zipFile.name}")
@@ -36,17 +32,13 @@ Gson().fromJson(root.resolve("packs.json").readText(), Array<PackEntry>::class.j
 }
 
 fun zipFolder(sourceFolder: Path, zipFile: Path) {
-    println("3")
     ZipOutputStream(zipFile.outputStream(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)).use { zipOut ->
         Files.walk(sourceFolder).forEach { filePath ->
-            println("4 $filePath")
             if (Files.isRegularFile(filePath)) {
-                println("5")
                 val zipEntry = ZipEntry(sourceFolder.relativize(filePath).toString())
                 zipOut.putNextEntry(zipEntry)
                 Files.copy(filePath, zipOut)
                 zipOut.closeEntry()
-                println("6")
             }
         }
     }
